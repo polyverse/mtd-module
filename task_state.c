1344bc9b757a2032f4ed09bb3a2c8c67bf03019f
Mariusz Borsa <wrotki@msn.com>
2020-02-01 16:42:19 -0800

    Return RIP in addition to RSP and RBP

--- task_state.c
+++ task_state.c
@@ -15,10 +15,15 @@ unsigned long KSTK_EBP_PRIVATE(struct task_struct *task)
 	return task_pt_regs(task)->bp;
 }
 
+unsigned long KSTK_EIP_PRIVATE(struct task_struct *task) 
+{
+	return task_pt_regs(task)->ip;
+}
+
 char *task_state(int pid, char* buf, size_t buf_size) 
 {
     struct task_struct *task = NULL;
-    unsigned long esp, ebp;
+    unsigned long esp, ebp, eip;
 
     task = pid_task(find_vpid(pid), PIDTYPE_PID);
     if (task == NULL) {
@@ -26,6 +31,7 @@ char *task_state(int pid, char* buf, size_t buf_size)
     }
     esp = KSTK_ESP_PRIVATE(task);
     ebp = KSTK_EBP_PRIVATE(task);
-    snprintf(buf, buf_size, "TASK STATE SP:\n%016lX %016lX", esp, ebp); 
+    eip = KSTK_EIP_PRIVATE(task);
+    snprintf(buf, buf_size, "TASK STATE SP:\n%016lX %016lX %016lX", esp, ebp, eip); 
     return buf;
 }
