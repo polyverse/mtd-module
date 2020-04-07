8f6e707195197fa6e8a712eefed42407237244e2
Thomas Oldham <toldham@uw.edu>
2020-04-07 07:23:26 -0700

    Do not publish image if git directory is not clean

--- publish.sh
+++ publish.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# Don't publish when there are uncommitted changes.
+if [ ! -z "$(git status -s -uno)" ]; then
+	echo "COMMIT IS DIRTY, aborting"
+	exit 1
+fi
+
 # need creds to be able to push to our ecr
 aws --region $AWS_DEFAULT_REGION ecr get-login  --no-include-email | bash -s
 
